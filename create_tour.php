157c1eb1ae338e049871eb1a59d564ff3ff45ea8
Sumindra Chaudhary <129476555+cdrysumindra110@users.noreply.github.com>
2024-11-20 23:30:56 +0545

    changes

--- create_tour.php
+++ create_tour.php
@@ -131,8 +131,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
     <link rel="stylesheet" href="owl-carousel/owl.theme.css">
     <!-- CUSTOM STYLE -->      
-    <link rel="stylesheet" href="css/template-style.css">
-    <link rel="stylesheet" href="css/tour_org.css">
+    <link rel="stylesheet" href="./css/template-style.css">
+    <link rel="stylesheet" href="./css/tour_org.css">
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&display=swap" rel="stylesheet">  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
