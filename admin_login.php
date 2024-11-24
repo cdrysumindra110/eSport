984a7a88b6b41359c424c969f519183850b0d12c
Sumindra Chaudhary <cdrysumindra110@gmail.com>
2024-11-24 22:27:49 +0545

    changes

---
+++ admin_login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>InfiKnight Admin Login Form</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Gaming Login Form Widget Tab Form,Login Forms,Sign up Forms,Registration Forms,News letter Forms,Elements"/>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="./css/admin-page.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+	<div class="padding-all">
+		<div class="header">
+			<h1><a href="index.php"><img src="./img/dash-logo.png" alt=" "></a> Admin Login Form</h1>
+		</div>
+
+		<div class="design-w3l">
+			<div class="mail-form-agile">
+				<form action="admin_login.php" method="post">
+					<input type="text" name="name" id="name" placeholder="Enter email..." required=""/>
+					<input type="password"  name="password" id="password" class="padding" placeholder="Enter Password" required=""/>
+					<input type="submit" value="submit">
+				</form>
+			</div>
+		  <div class="clear"> </div>
+		</div>
+		
+		<div class="footer">
+		<p>© 2022 All Rights Reserved | Design by Team <a href="https://sumindra14.com.np/" > &infin; InfiKnight </a></p>
+		</div>
+	</div>
+</body>
+</html>
